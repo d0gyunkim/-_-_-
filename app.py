ccd13d876feb1cc04f7a2639bd3c253e988ab3b6
d0gyunkim <herry0515@naver.com>
2024-11-07 18:49:57 +0900

    g

--- app.py
+++ app.py
@@ -7,12 +7,11 @@ header = {
     "project": "KHU_PROMPTHON_018", 
     "apiKey": "프로젝트의 API KEY" 
    }
-
-
-URL = "https//"+os.getenv("URL")
-response = requests.post(URL, headers=header, json=body)
-
 body = {
      "hash": "2cc6f00d01058516d8d0ac3b062a2547679aaef311d22b82400aa82f8ef4be47", 
     "params": {"task":""} 
    }
+
+URL = os.getenv("URL")
+response = requests.post(URL, headers=header, json=body)
+
