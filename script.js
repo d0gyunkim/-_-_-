15a601ec9529ba7072c4862731b43699b82df5b3
d0gyunkim <herry0515@naver.com>
2024-11-07 18:47:37 +0900

    bg

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
+
+
+
+
+
 function getRecommendation() {
     // 사용자가 입력한 값을 가져옴
     const cuisine = document.getElementById('cuisine').value;
