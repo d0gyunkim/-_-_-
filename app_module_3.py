c5de17fe9130c0adf68cf254bffdb7a6696962a5
d0gyunkim <herry0515@naver.com>
2024-11-09 16:13:32 +0900

    ㄹ

--- app_module_3.py
+++ app_module_3.py
@@ -1,35 +1,37 @@
-# app_module_3.py: 장학팀에 관한 정보(메일,전화번호,팩스번호)등을 처리하는 함수
+# 장학팀에 관한 정보를 내보내주는 모듈
+
+
+import os
 import requests
 from dotenv import load_dotenv
-import os
-import json
 
-def handle_content(con):
-    # 환경 변수 로드
-    load_dotenv()
+# 환경 변수 로드
+load_dotenv()
 
-    # Header 설정
-    header = {
-        "project": "KHU_PROMPTHON_018", 
-        "apiKey": os.getenv("WANTED_API_KEY")
-    }
+class Module3Handler:
+    def __init__(self):
+        self.header = {
+            "project": "KHU_PROMPTHON_018",
+            "apiKey": os.getenv("WANTED_API_KEY")
+        }
+        self.url = os.getenv("WANTED_API_URL")
 
-    body = {
-      "hash": "c0d17e57929ea2d5d3c20361c8274a99ab2178d4907fe04542fc31c9f5987b1d", 
-       "params": {"question":con,"campus":""} 
-}
-
-    # API URL 가져오기
-    URL = os.getenv("WANTED_API_URL")
-
-    # POST 요청
-    response = requests.post(URL, headers=header, json=body)
-
-    # 응답 출력
-    if response.ok:
-        content = response.json()["choices"][0]["message"]["content"]
-        print("Handled Content in Module 3:", content)
-        print("body: ",body)
-    else:
-        print("Response status code:", response.status_code)
-        print("Response content:", response.text)
+    def handle_content(self, content):
+        # 장학금 관련 질문 처리
+        body = {
+            "hash": "c0d17e57929ea2d5d3c20361c8274a99ab2178d4907fe04542fc31c9f5987b1d",
+            "params": {
+                "question": content,
+                "campus": "",
+                "today_datetime": "11/8/2024 3pm"
+            }
+        }
+        
+        response = requests.post(self.url, headers=self.header, json=body)
+        
+        if response.ok:
+            response_content = response.json()["choices"][0]["message"]["content"]
+            print("Handled Content in Module 3:", response_content)
+        else:
+            print("Response status code:", response.status_code)
+            print("Response content:", response.text)
